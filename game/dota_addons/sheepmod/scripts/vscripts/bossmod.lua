58fcee780e2f133190ddd543741b93e4cf830a90
Jkb6873 <jakub.taraska96@gmail.com>
2017-12-21 23:43:43 -0500

    Half of win conditions set

--- game/dota_addons/sheepmod/scripts/vscripts/bossmod.lua
+++ game/dota_addons/sheepmod/scripts/vscripts/bossmod.lua
@@ -10,31 +10,18 @@ end
 
 function bossmod:DeclareFunctions()
 	local funcs = {
-		MODIFIER_EVENT_ON_TAKEDAMAGE
+		MODIFIER_EVENT_ON_DEATH
 	}
 	return funcs
 end
 
-function bossmod:OnTakeDamage(params)
-	PrintTable("OnTakeDamage",params)
---	if self:GetParent():PassivesDisabled() then
---		return
---	end
---	local return_damage 
---	if RollPercentage(self:GetAbility():GetSpecialValueFor("full_damage_chance")) then
---		return_damage = params.original_damage
---		SendOverheadEventMessage(nil, OVERHEAD_ALERT_DAMAGE, params.attacker, return_damage, nil)
---	else
---		return_damage = self:GetAbility():GetSpecialValueFor("damage_return")*0.01*params.original_damage
---	end
---		
---	ApplyDamage(
---	{
---		victim = params.attacker, 
---		attacker = params.unit, 
---		damage = return_damage, 
---		damage_type = DAMAGE_TYPE_MAGICAL,
---		damage_flags = DOTA_DAMAGE_FLAG_REFLECTION,
---		ability = self:GetAbility()
---	})
+function bossmod:OnDeath(params)
+	if not (params.unit == self:GetParent()) then
+		return
+	end
+	if self:GetParent():GetTeam() == DOTA_TEAM_GOODGUYS then
+		GameRules:SetGameWinner(DOTA_TEAM_BADGUYS)
+	else
+		GameRules:SetGameWinner(DOTA_TEAM_GOODGUYS)
+	end
 end			
